6504800c57c800f3735dedf5c7f6747ea98c37ea
wanzzz-05 <pawan.karu2005@gmail.com>
2025-08-07 18:41:47 +0530

    Changed API_URL

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/item";
+const API_URL = "https://shopping-list-backend-production.up.railway.app/item";
 
 export const getItems = async ()=> {
     try {
